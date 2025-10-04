a8106e85bbddb57c8f2b6f5b7328a85ccf315bd4
板谷 竹 <itaya-t@rits.local>
2025-10-04 11:15:20 +0900

    Update requirements for Streamlit Cloud

--- trig_quiz_app_on_web.py
+++ trig_quiz_app_on_web.py
@@ -103,7 +103,7 @@ if st.session_state.question_number > 10:
     st.write(f"経過時間: {elapsed} 秒")
 
     # LaTeX 表で表示（先頭列に問題番号）
-    latex_table = r"\def\arraystretch{2.5}\begin{array}{|c|c|c|c|c|} \hline 番号. & 問題 & あなたの解答 & 正解 & 正誤 \\ \hline "
+    latex_table = r"\def\arraystretch{3}\begin{array}{|c|c|c|c|c|} \hline 番号. & 問題 & あなたの解答 & 正解 & 正誤 \\ \hline "
     for i, a in enumerate(st.session_state.answers, 1):
         mark = "○" if a["user"] == a["correct"] else "×"
         latex_table += f"{i} & {a['problem']} & {a['user']} & {a['correct']} & {mark} \\\\ \hline "
